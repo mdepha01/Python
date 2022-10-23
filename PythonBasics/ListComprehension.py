9cba1e1dfcd5c02a867cb48800bd9e9824c66b5c
mdepha01 <93098706+mdepha01@users.noreply.github.com>
2022-10-23 02:50:36 +0200

    Update ListComprehension.py

--- PythonBasics/ListComprehension.py
+++ PythonBasics/ListComprehension.py
@@ -4,3 +4,11 @@
 var1, var2 = [int (x) for x in input("Please enter Two numbers\n").split()]
 print("First number : ", var1)
 print("Second number : ", var2)
+
+"""
+when seperating numbers using a comma
+
+var1, var2 = [int(x) for x in input("Please enter Two numbers\n").split(",")]
+print("First number : ", var1)
+print("Second number : ", var2)
+"""
