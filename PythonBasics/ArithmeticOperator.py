abc247d770a63788ac162943fa10653f9d6b3a45
mdepha01 <93098706+mdepha01@users.noreply.github.com>
2022-10-23 18:54:51 +0200

    Create ArithmeticOperator.py

---
+++ PythonBasics/ArithmeticOperator.py
@@ -0,0 +1,29 @@
+# Arithmetic operators in action
+
+a = 10
+b = 3
+
+# addition
+add = a + b
+
+# subtraction
+diff = a - b
+
+# multiplication
+prod = a * b
+
+# division
+quotient = a / b
+
+# Remainder
+rem = a % b
+
+# Exponent
+exp = a**b
+
+print("The sum of {} and {} : {}".format(a, b, add))
+print("The difference of {} and {} : {}".format(a, b, diff))
+print("Product of {} and {} : {}".format(a, b, prod))
+print("The Quotient of {} and {} : {}".format(a, b, quotient))
+print("The remainder of {} and {} : {}".format(a, b, rem))
+print("{} power {} is : {}".format(a, b, exp))
