ad8216afeb06a3509d7dcf7228df5bd4cc0d9b98
mdepha01 <93098706+mdepha01@users.noreply.github.com>
2022-10-23 02:17:12 +0200

    Create MultipleInputs.py

---
+++ PythonBasics/MultipleInputs.py
@@ -0,0 +1,7 @@
+# This program takes multiple inputs from user using split() method.
+# Displays all the input values on console.
+
+# Takes to input values from user and displays them on console
+var1, var2 = input("Please enter two values\n").split()
+print("first number:", var1)
+print("second number:", var2)
